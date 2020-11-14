8aad3ce636bfedc74412939c3a80e3e7a2b5cf3b
sajka <sajkarp@mail.ru>
2020-11-14 19:35:52 +0300

    Make UI Details Movies

--- app/src/main/java/by/app/slise/MainActivity.kt
+++ app/src/main/java/by/app/slise/MainActivity.kt
@@ -1,11 +1,21 @@
 package by.app.slise
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.TextMain)
+        textView.setOnClickListener { moveToNextScreen() }
+    }
+
+    private fun moveToNextScreen(){
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
