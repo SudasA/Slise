9388c74f26e65e92098cee8814a443a1e5c7f313
sajka <Sudasa1989>
2021-01-10 17:44:10 +0300

    change fragment

--- app/src/main/java/by/app/slise/MainActivity.kt
+++ app/src/main/java/by/app/slise/MainActivity.kt
@@ -3,7 +3,7 @@ package by.app.slise
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import by.app.slise.MovieDetails.FragmentMoviesDetails
+import by.app.slise.MovieList.MoviesFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, FragmentMoviesDetails())
+                .replace(R.id.container, MoviesFragment())
                 .commitNow()
         }
     }
